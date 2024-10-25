d87689e667fb690af59024016d65165c34a8521d
Owen Richards <owen@biglemon.co.uk>
2024-10-25 12:39:02 +0100

    feat: executable script to demo the system

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { Logging } from "./lib/Logging";
+import { Parse } from "./lib/Parse";
+import { thing } from "./models/Thing";
+
+const run = () => {
+  try {
+    // Parse the CSV file
+    const data = Parse.importFromCSV("things.csv");
+
+    // Validate the data and store it
+    const validatedData = thing.validate(data);
+    thing.insertMany(validatedData);
+
+    // Query the data
+    const results = thing.findMany({
+      where: {
+        id: {
+          lte: 2,
+        },
+      },
+      select: {
+        id: true,
+        name: true,
+      },
+    });
+
+    // Log the data to the console
+    Logging.logToConsole(results);
+  } catch (error: unknown) {
+    Logging.handleUnknownError(error);
+  }
+};
+
+run();
